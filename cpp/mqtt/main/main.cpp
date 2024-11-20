d1b2d0cef6bb63d38d7dbf57e2cb34a4d08fc5e8
montana <montanagau13@gmail.com>
2024-11-19 18:47:47 -0700

    added mqtt client, tested and working

--- cpp/mqtt/main/main.cpp
+++ cpp/mqtt/main/main.cpp
@@ -1,17 +1,18 @@
 #include "WifiService.h"
+#include "MqttClient.h"
 
 extern "C"
 {
-#include <stdio.h>
-#include <esp_err.h>
-#include <esp_log.h>
-#include <nvs_flash.h>
-#include <esp_wifi.h>
-#include <mqtt_client.h>
-#include <esp_event.h>
+    #include <stdio.h>
+    #include <esp_err.h>
+    #include <esp_log.h>
+    #include <nvs_flash.h>
+    #include <esp_wifi.h>
+    #include <mqtt_client.h>
+    #include <esp_event.h>
 }
 
-static char *TAG = "MQTT_CLIENT";
+static char *TAG = "mqtt client";
 static char *SSID = "IP-in-the-hot-tub";
 static char *PASSWORD = "everytime";
 
@@ -55,70 +56,15 @@ void wifiInit()
                                PASSWORD});
 }
 
-static void log_error_if_nonzero(const char *message, int error_code)
+void onConnect(Mqtt::MqttClient* client)
 {
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
-    }
+    ESP_LOGI(TAG, "connected to broker");
 }
 
-void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+void onDisconnect(Mqtt::MqttClient* client)
 {
-    ESP_LOGI(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
-    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-
-    switch ((esp_mqtt_event_id_t)event_id)
-    {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data1234", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
-        {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-        }
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
-    }
+    ESP_LOGI(TAG, "disconnected from broker");
+    // We could implement reconnect logic here
 }
 
 extern "C" int app_main(void)
@@ -130,22 +76,74 @@ extern "C" int app_main(void)
     ESP_LOGI(TAG, "waiting for wifi to connect");
     WifiService::waitConnectionState({ConnectionState::CONNECTED});
 
-    // Create the mqtt configuration
-    esp_mqtt_client_config_t mqtt_cfg = {
-        .broker = {
-            .address = {
-                .uri = "mqtt://test.mosquitto.org",
-                // .uri = "mqtt://mqtt.eclipseprojects.io",
-            }}};
+    // Create the mqtt client
+    Mqtt::MqttClient client("mqtt://test.mosquitto.org");
+    
+    // Set the onConnected delegate
+    client.onConnected = onConnect;
 
-    // Initialize the mqtt client
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-
-    // Register the event handler
-    esp_mqtt_client_register_event(client, MQTT_EVENT_ANY, mqtt_event_handler, NULL);
+    // Set the onDisconnected delegate
+    client.onDisconnected = onDisconnect;
 
     // Start the mqtt client
-    esp_mqtt_client_start(client);
+    client.connect();
+
+    // Wait for the mqtt client to connect
+    ESP_LOGI(TAG, "waiting for mqtt client to connect");
+    client.waitConnectionState({Mqtt::ConnectionState::CONNECTED});
+
+    // Subscribe to a topic
+    int messageId = client.subscribe("/topic/qos0", [](Mqtt::MqttClient* client, int messageId, std::vector<std::byte> data) -> void {
+        ESP_LOGI(TAG, "message received");
+
+        // Convert the data to a string
+        std::string message(reinterpret_cast<const char*>(data.data()), data.size());
+
+        // Print out each byte
+        for (std::byte b : data)
+        {
+            ESP_LOGI(TAG, "byte: %d", static_cast<int>(b));
+        }
+
+        const char* cStr = message.c_str();
+        // Print out each character until the null terminator
+        for (int i = 0; cStr[i] != '\0'; i++)
+        {
+            ESP_LOGI(TAG, "char: %c", cStr[i]);
+        }
+
+        // Log the message
+        ESP_LOGI(TAG, "message: %d, %s", data.size(), message.c_str());
+    });
+    ESP_LOGI(TAG, "subscribed to topic, message id: %d", messageId);
+
+    // Publish a byte vector message to a topic
+    std::string message = "hello";
+    std::vector<std::byte> data(message.size());
+    std::transform(message.begin(), message.end(), data.begin(),
+                [](char c) { return static_cast<std::byte>(c); });
+    int msg1Id = client.publish("/topic/qos0", data);
+    ESP_LOGI(TAG, "published message, message id: %d", msg1Id);
+
+    // Delay for a second (this seems to be somewhat necessary for such quick publishes back-to-back)
+    ESP_LOGI(TAG, "delaying for a few seconds");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // Publish a string message to a topic
+    int msg2Id = client.publish("/topic/qos0", "world");
+    ESP_LOGI(TAG, "published message, message id: %d", msg2Id);
+
+    // Delay for a second
+    ESP_LOGI(TAG, "delaying for a few seconds again");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // Unsubscribe from a topic
+    messageId = client.unsubscribe("/topic/qos0");
+    ESP_LOGI(TAG, "unsubscribed from topic, message id: %d", messageId);
+
+    // Disconnect the mqtt client
+    ESP_LOGI(TAG, "disconnecting from broker");
+    client.disconnect();
 
     // Wait indefinitely
     while (true)
