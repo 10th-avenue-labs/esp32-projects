cd0fbd63c0ffcd9e286c45981c21a1949e310478
montana <montanagau13@gmail.com>
2024-12-08 17:01:42 -0700

    Refactor plug config to use result Add doc Allow for nullability of plug config members on deserialization

--- cpp/smart-plug/peripheral/components/PlugConfig/PlugConfig.cpp
+++ cpp/smart-plug/peripheral/components/PlugConfig/PlugConfig.cpp
@@ -1,8 +1,8 @@
 #include "PlugConfig.h"
 
-const char* TAG = "PLUG_CONFIG";
+static const char* TAG = "PLUG_CONFIG";
 
-tuple<esp_err_t, unique_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string& namespaceValue, const string& key) {
+Result<shared_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string& namespaceValue, const string& key) {
     // Initialise the non-volatile flash storage (NVS)
     ESP_LOGI(TAG, "initializing nvs flash");
     esp_err_t response = nvs_flash_init();
@@ -21,8 +21,7 @@ tuple<esp_err_t, unique_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string
             response = nvs_flash_init();
 
             if (response != ESP_OK) {
-                ESP_LOGE(TAG, "failed to initialize nvs flash, error code: %d ", response);
-                return {response, nullptr};
+                return Result<shared_ptr<PlugConfig>>::createFailure(format("failed to initialize nvs flash: {}", esp_err_to_name(response)));
             };
         }
     }
@@ -35,8 +34,7 @@ tuple<esp_err_t, unique_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string
         &error                  // Error code
     );
     if (error != ESP_OK) {
-        ESP_LOGE(TAG, "failed to open nvs handle, error code: %d", error);
-        return {error, nullptr};
+        return Result<shared_ptr<PlugConfig>>::createFailure(format("failed to open nvs handle: {}", esp_err_to_name(error)));
     }
 
     // Read the size of the serialized plug configuration from the NVS
@@ -48,24 +46,21 @@ tuple<esp_err_t, unique_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string
         case ESP_ERR_NVS_NOT_FOUND: {
             ESP_LOGI(TAG, "the key '%s' is not initialized yet", key.c_str());
 
-            return {ESP_OK, nullptr};
+            return Result<shared_ptr<PlugConfig>>::createSuccess(nullptr);
             break;
         }
         default:
-            ESP_LOGE(TAG, "error reading blob size: %s", esp_err_to_name(error));
-            return {error, nullptr};
+            return Result<shared_ptr<PlugConfig>>::createFailure(format("failed to read blob size: {}", esp_err_to_name(error)));
     }
 
     // Read the serialized plug configuration from the NVS
     void* blob = malloc(size + 1); // We add 1 to fit the null terminator at the end
     error = handle->get_blob(key.c_str(), blob, size);
     if (error != ESP_OK) {
-        ESP_LOGE(TAG, "failed to read blob with key '%s' from nvs, error code: %s", key.c_str(), esp_err_to_name(error));
-        return {error, nullptr};
+        return Result<shared_ptr<PlugConfig>>::createFailure(format("failed to read blob with key '{}' from nvs: {}", key.c_str(), esp_err_to_name(error)));
     }
     if (blob == nullptr) {
-        ESP_LOGE(TAG, "failed to read blob from nvs");
-        return {ESP_FAIL, nullptr};
+        return Result<shared_ptr<PlugConfig>>::createFailure("failed to read blob from nvs");
     }
 
     // Add the null terminator to the string
@@ -74,7 +69,39 @@ tuple<esp_err_t, unique_ptr<PlugConfig>> PlugConfig::readPlugConfig(const string
     // Deserialize the plug configuration
     PlugConfig plugConfig = PlugConfig::deserialize(string((char*)blob)); 
 
-    return {ESP_OK, make_unique<PlugConfig>(plugConfig)};
+    return Result<shared_ptr<PlugConfig>>::createSuccess(make_shared<PlugConfig>(plugConfig));
+}
+
+Result<> PlugConfig::writePlugConfig(const string& namespaceValue, const string& key, const PlugConfig& plugConfig) {
+    // TODO: Should probably init nvs
+
+    // Get a handle to the Namespace in the NVS
+    esp_err_t error;
+    unique_ptr<nvs::NVSHandle> handle = nvs::open_nvs_handle(
+        namespaceValue.c_str(), // Namespace
+        NVS_READWRITE,          // Open mode
+        &error                  // Error code
+    );
+    if (error != ESP_OK) {
+        return Result<>::createFailure(format("failed to open nvs handle: {}", esp_err_to_name(error)));
+    }
+
+    // Serialize the plug configuration
+    string serialized = cJSON_Print(serialize().get());
+
+    // Write the serialized plug configuration to the NVS
+    error = handle->set_blob(key.c_str(), serialized.c_str(), serialized.length());
+    if (error != ESP_OK) {
+        return Result<>::createFailure(format("failed to write blob with key '{}' to nvs: {}", key.c_str(), esp_err_to_name(error)));
+    }
+
+    // Commit the changes
+    error = handle->commit();
+    if (error != ESP_OK) {
+        return Result<>::createFailure(format("failed to commit changes to nvs: {}", esp_err_to_name(error)));
+    }
+
+    return Result<>::createSuccess();
 }
 
 unique_ptr<cJSON, void (*)(cJSON *item)> PlugConfig::serialize() {
@@ -82,10 +109,10 @@ unique_ptr<cJSON, void (*)(cJSON *item)> PlugConfig::serialize() {
     unique_ptr<cJSON, decltype(&cJSON_Delete)> root(cJSON_CreateObject(), cJSON_Delete);
 
     // Serialize the object
-    cJSON_AddItemToObject(root.get(), "bleConfig", bleConfig->serialize().release());
-    cJSON_AddItemToObject(root.get(), "acDimmerConfig", acDimmerConfig->serialize().release());
-    cJSON_AddItemToObject(root.get(), "wifiConfig", wifiConfig->serialize().release());
-    cJSON_AddItemToObject(root.get(), "mqttConfig", mqttConfig->serialize().release());
+    cJSON_AddItemToObject(root.get(), "bleConfig", bleConfig == nullptr ? cJSON_CreateNull() : bleConfig->serialize().release());
+    cJSON_AddItemToObject(root.get(), "acDimmerConfig", acDimmerConfig == nullptr ? cJSON_CreateNull() : acDimmerConfig->serialize().release());
+    cJSON_AddItemToObject(root.get(), "wifiConfig", wifiConfig == nullptr ? cJSON_CreateNull() : wifiConfig->serialize().release());
+    cJSON_AddItemToObject(root.get(), "mqttConfig", mqttConfig == nullptr ? cJSON_CreateNull() : mqttConfig->serialize().release());
 
     return root;
 }
@@ -99,10 +126,14 @@ PlugConfig PlugConfig::deserialize(const string& serialized) {
     unique_ptr<cJSON, decltype(&cJSON_Delete)> root(cJSON_Parse(serialized.c_str()), cJSON_Delete);
 
     // Deserialize the object
-    plugConfig.bleConfig = make_shared<BleConfig>(BleConfig::deserialize(cJSON_Print(cJSON_GetObjectItem(root.get(), "bleConfig"))));
-    plugConfig.acDimmerConfig = make_shared<AcDimmerConfig>(AcDimmerConfig::deserialize(cJSON_Print(cJSON_GetObjectItem(root.get(), "acDimmerConfig"))));
-    plugConfig.wifiConfig = make_shared<WifiConfig>(WifiConfig::deserialize(cJSON_Print(cJSON_GetObjectItem(root.get(), "wifiConfig"))));
-    plugConfig.mqttConfig = make_shared<MqttConfig>(MqttConfig::deserialize(cJSON_Print(cJSON_GetObjectItem(root.get(), "mqttConfig"))));
+    auto bleConfig = cJSON_GetObjectItem(root.get(), "bleConfig");
+    plugConfig.bleConfig = cJSON_IsNull(bleConfig) ? nullptr : make_shared<BleConfig>(BleConfig::deserialize(cJSON_Print(bleConfig)));
+    auto acDimmerConfig = cJSON_GetObjectItem(root.get(), "acDimmerConfig");
+    plugConfig.acDimmerConfig = cJSON_IsNull(acDimmerConfig) ? nullptr : make_shared<AcDimmerConfig>(AcDimmerConfig::deserialize(cJSON_Print(acDimmerConfig)));
+    auto wifiConfig = cJSON_GetObjectItem(root.get(), "wifiConfig");
+    plugConfig.wifiConfig = cJSON_IsNull(wifiConfig) ? nullptr : make_shared<WifiConfig>(WifiConfig::deserialize(cJSON_Print(wifiConfig)));
+    auto mqttConfig = cJSON_GetObjectItem(root.get(), "mqttConfig");
+    plugConfig.mqttConfig = cJSON_IsNull(mqttConfig) ? nullptr : make_shared<MqttConfig>(MqttConfig::deserialize(cJSON_Print(mqttConfig)));
 
     return plugConfig;
 };
--- cpp/smart-plug/peripheral/components/PlugConfig/include/PlugConfig.h
+++ cpp/smart-plug/peripheral/components/PlugConfig/include/PlugConfig.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <nvs_handle.hpp>
 #include "cJSON.h"
+#include "Result.h"
 #include "AcDimmerConfig.h"
 #include "WifiConfig.h"
 #include "BleConfig.h"
@@ -17,14 +18,43 @@ using namespace std;
 
 class PlugConfig {
     public:
-        shared_ptr<BleConfig> bleConfig;
-        shared_ptr<AcDimmerConfig> acDimmerConfig;
-        shared_ptr<WifiConfig> wifiConfig;
-        shared_ptr<MqttConfig> mqttConfig;
+        shared_ptr<BleConfig> bleConfig;            // Config for BLE
+        shared_ptr<AcDimmerConfig> acDimmerConfig;  // Config for the ac dimmer
+        shared_ptr<WifiConfig> wifiConfig;          // Config for wifi
+        shared_ptr<MqttConfig> mqttConfig;          // Config for mqtt
 
-        static tuple<esp_err_t, unique_ptr<PlugConfig>> readPlugConfig(const string& namespaceValue, const string& key);
+        /**
+         * @brief Read a plug configuration from NVS returning a deserialized PlugConfig object
+         * 
+         * @param namespaceValue The namespace to read from
+         * @param key The key in the namespace to read from
+         * @return Result<shared_ptr<PlugConfig>> A result containing the deserialized PlugConfig object
+         */
+        static Result<shared_ptr<PlugConfig>> readPlugConfig(const string& namespaceValue, const string& key);
 
+        /**
+         * @brief Write a plug config to NVS
+         * 
+         * @param namespaceValue The namespace to write to
+         * @param key The key in the namespace to write to
+         * @param plugConfig The plug config to write
+         * @return Result<> A result indicating success or failure
+         */
+        Result<> writePlugConfig(const string& namespaceValue, const string& key, const PlugConfig& plugConfig);
+
+        /**
+         * @brief Serialize the plug config to a cJSON object
+         * 
+         * @return unique_ptr<cJSON, void (*)(cJSON *item)> A unique pointer to the cJSON object
+         */
         unique_ptr<cJSON, void (*)(cJSON *item)> serialize();
+
+        /**
+         * @brief Deserialize a plug config from a serialized string
+         * 
+         * @param serialized The serialized string
+         * @return PlugConfig The deserialized plug config
+         */
         static PlugConfig deserialize(const string& serialized);
 };
 
