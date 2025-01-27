e5b714c6b0df82e7e9d8cd8447e5ae7d0a7fdb29
Montana Gau <montanagau13@gmail.com>
2025-01-26 21:43:17 -0700

    added waiter component stub, component tests, documentation updates, and .vscode setting fixes

---
+++ cpp/component-tests/Waiter/main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#include "Waiter.h"
+
+extern "C" void app_main()
+{
+    Waiter();
+}
---
+++ cpp/components/Waiter/include/Waiter.h
@@ -0,0 +1,16 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+#include "Result.h"
+
+using namespace std;
+
+static const char *RESULT_TAG = "RESULT";
+
+class Waiter
+{
+public:
+    Waiter() {};
+};
+
+#endif // RESULT_H
