fa775a46f3048f5d4b63d697ac20e5e5f5aa62be
Montana Gau <montanagau13@gmail.com>
2025-02-10 22:56:15 -0700

    Added ISerializable test

--- cpp/components/ISerializable/include/ISerializable.h
+++ cpp/components/ISerializable/include/ISerializable.h
@@ -2,12 +2,35 @@
 #define ISERIALIZABLE_H
 
 #include <string>
+#include <memory>
+#include "cJSON.h"
 
 using namespace std;
 
-class ISerializable {
-    public:
-        virtual string serialize() = 0;
+class ISerializable
+{
+public:
+    /**
+     * @brief Serialize the object to a cJSON object
+     *
+     * @return unique_ptr<cJSON, void (*)(cJSON *item)> The serialized cJSON object
+     */
+    virtual unique_ptr<cJSON, void (*)(cJSON *item)> serialize() = 0;
+
+    /**
+     * @brief Serialize the object to a string
+     *
+     * @param pretty Whether to pretty-print the JSON
+     * @return string The serialized JSON string
+     */
+    string serializeToString(bool pretty = false)
+    {
+        unique_ptr<cJSON, void (*)(cJSON *item)> json(serialize().release(), cJSON_Delete);
+        char *jsonStr = pretty ? cJSON_Print(json.get()) : cJSON_PrintUnformatted(json.get());
+        string result(jsonStr);
+        free(jsonStr);
+        return result;
+    }
 };
 
 #endif // ISERIALIZABLE_H
