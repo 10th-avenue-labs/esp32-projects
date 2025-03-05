96f806b0566f70704e0aa0f5efabf0499a55b3b3
Montana Gau <montanagau13@gmail.com>
2025-03-04 22:09:56 -0700

    pulling from mega-branch 'create-smart-device-component'

--- cpp/components/ISerializable/include/ISerializable.h
+++ cpp/components/ISerializable/include/ISerializable.h
@@ -1,13 +1,31 @@
-#ifndef ISERIALIZABLE_H
-#define ISERIALIZABLE_H
+#pragma once
 
 #include <string>
+#include <memory>
+#include "cJSON.h"
 
-using namespace std;
+class ISerializable
+{
+public:
+    /**
+     * @brief Serialize the object to a cJSON object
+     *
+     * @return std::unique_ptr<cJSON, void (*)(cJSON *item)> The serialized cJSON object
+     */
+    virtual std::unique_ptr<cJSON, void (*)(cJSON *item)> serialize() = 0;
 
-class ISerializable {
-    public:
-        virtual string serialize() = 0;
+    /**
+     * @brief Serialize the object to a string
+     *
+     * @param pretty Whether to pretty-print the JSON
+     * @return string The serialized JSON string
+     */
+    std::string serializeToString(bool pretty = false)
+    {
+        std::unique_ptr<cJSON, void (*)(cJSON *item)> json(serialize().release(), cJSON_Delete);
+        char *jsonStr = pretty ? cJSON_Print(json.get()) : cJSON_PrintUnformatted(json.get());
+        std::string result(jsonStr);
+        free(jsonStr);
+        return result;
+    }
 };
-
-#endif // ISERIALIZABLE_H
